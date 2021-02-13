0362d6e742546965522586a33756dfc58ee0ce7d
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2021-02-13 16:41:38 +1100

    added User, bcrypt and session mgmt

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,19 @@
 class PagesController < ApplicationController
   def home
+    cookies[:fave_drink] = "beer" 
+    cookies[:fave_color] = "blue"
+    cookies[:fave_foods] = {:value => ["pizza","cheesecake","vindaloo"], :expires => Time.now + 600 }
+    
+    cookies[:count] = cookies[:count] ? cookies[:count].to_i + 1 : 1
+    @count = cookies[:count]
+    @fave_drink = cookies[:fave_drink]
+    @fave_foods = cookies[:fave_foods] if cookies[:fave_foods]
+    @fave_color = cookies[:fave_color]
+    
+    
   end
 
   def restricted
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/restricted'
+  resources :users
+  root 'pages#home'
+  get '/signup', to: 'users#new', as: 'signup'
+  get '/signin', to: 'sessions#new', as: 'signin'
+  post '/signin', to: 'sessions#create', as: 'create_session'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
